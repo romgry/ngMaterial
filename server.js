3eddbf0c72f4b47cd605538defb17d840329e205
Gary Romanov <gary@mdisoft.com>
2016-09-15 23:55:11 -0400

    mongoose

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://heroku_qcn57j1p:ppu6jlegk81midn7fev9mjbj0g@ds033036.mlab.com:33036/heroku_qcn57j1p')
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
@@ -14,6 +16,31 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//db connections
+// MongoDB via Mongoose
+var db = mongoose.connection;
+db.on('connecting', function() {
+    log.info('MongoDB connecting...');
+});
+db.on('error', function(error) {
+    log.error('MongoDB connection error: ' + error);
+    mongoose.disconnect();
+});
+db.on('connected', function() {
+    log.info('MongoDB connected to: mongodb://%s:%s/%s', db.host, db.port, db.name);
+});
+db.once('open', function() {
+    log.info('MongoDB connection opened');
+});
+db.on('reconnected', function () {
+    log.info('MongoDB reconnected');
+});
+db.on('disconnected', function() {
+    log.error('MongoDB disconnected !');
+    mongoose.connect(models.MONGODB_SERVER, models.MONGODB_SERVER_OPTIONS);
+});
+mongoose.connect(models.MONGODB_SERVER, models.MONGODB_SERVER_OPTIONS);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
